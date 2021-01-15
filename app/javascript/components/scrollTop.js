c5297eb967c8d4270ef5f471029193f723a6cb0f
“LLINLU” <linlu96@yahoo.com>
2021-01-16 01:00:20 +0900

    done

--- app/javascript/components/scrollTop.js
+++ app/javascript/components/scrollTop.js
@@ -4,6 +4,17 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+ $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 30) {
+       btn.addClass('show-footer');
+  } else {
+    btn.removeClass('show-footer');
+
+   }
+});
 }
 
 export { scrollTop }
+
+
