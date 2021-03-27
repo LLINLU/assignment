5603e69e2ad307e4ab718e1589d50ffe60d6ff60
linlu liu <linlu96@yahoo.com>
2021-03-27 23:56:55 +0900

    d

--- app/javascript/components/birdSlide.js
+++ app/javascript/components/birdSlide.js
@@ -1,7 +1,25 @@
 const birdAppear = () => {
   // const num = document.querySelector ('.js-number');
   const bird = document.querySelector('.self-picture');
+  const picture = documnet.querySelector('.hima');
   if(bird){
+
+
+  let birdPosition = bird.getBoundingClientRect().top;
+
+  let screenPosition = window.innerHeight / 2;
+
+    if (birdPosition < screenPosition) {
+
+     bird.classList.add('intro-appear');
+    }
+    else if (birdPosition >= screenPosition && true) {
+      bird.classList.remove('intro-appear');
+    }
+  }
+
+
+   if(bird){
 
 
   let birdPosition = bird.getBoundingClientRect().top;
