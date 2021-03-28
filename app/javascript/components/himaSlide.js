ca8ebd6dc7c4281f6c960d38029472005f9688dd
linlu liu <linlu96@yahoo.com>
2021-03-28 21:53:21 +0900

    d

--- app/javascript/components/himaSlide.js
+++ app/javascript/components/himaSlide.js
@@ -1,1 +1,29 @@
-himaSlide.js
+const himaAppear = () => {
+  // const num = document.querySelector ('.js-number');
+  const bird = document.querySelector('.hima');
+  if(bird){
+
+
+  let birdPosition = bird.getBoundingClientRect().top;
+
+  let screenPosition = window.innerHeight / 1.1;
+
+    if (birdPosition < screenPosition) {
+
+     bird.classList.add('intro-appear');
+    }
+    else if (birdPosition >= screenPosition && true) {
+      bird.classList.remove('intro-appear');
+    }
+  }
+
+
+
+
+
+}
+
+
+window.addEventListener('scroll', himaAppear)
+
+export {himaAppear}
