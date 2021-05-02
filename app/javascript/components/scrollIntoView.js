78e0b718cb6624bad656d6c09070655bc0a956ff
linlu liu <linlu96@yahoo.com>
2021-05-02 18:38:11 +0900

    d

--- app/javascript/components/scrollIntoView.js
+++ app/javascript/components/scrollIntoView.js
@@ -1,6 +1,8 @@
 const scrollIntoView = () => {
   const arrow = document.querySelector ('.arrow_down');
   const worksection = document.querySelector('#work');
+  const down = document.querySelector('.downward');
+  const introsection = document.querySelector('#intro');
 
 if (arrow) {
 
@@ -10,7 +12,20 @@ arrow.addEventListener("click", () => {
 
 
 });
-}
+
+};
+
+if (down) {
+down.addEventListener("click", () => {
+  introsection.scrollIntoView({behavior:"smooth"});
+
+
+
+});
+
+
+
+};
 }
 
 export { scrollIntoView }
