2525b0ecd37f91327f038891d08194b1d8915ba3
linlu liu <linlu96@yahoo.com>
2021-05-02 19:35:30 +0900

    d

--- app/javascript/components/typeWriterEffect.js
+++ app/javascript/components/typeWriterEffect.js
@@ -1,10 +1,18 @@
 const typeWriterEffect = () => {
 
-let  words = ['think','design', 'code'];
+let  words = ['think', 'create', 'code'];
 let count = 0;
 let letter = 0;
 let wordToPrint = "";
 
+let inputWords = document.querySelector('#inputWords')
 
+if (inputWords) {
 
+const cycle = () => {
+  let word = words[count];
+  if ()
+
+}
+}
 }
