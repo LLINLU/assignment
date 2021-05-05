1ab9eba33126d22f51a81120a14c4af5f89ece5c
linlu liu <linlu96@yahoo.com>
2021-05-05 20:51:42 +0900

    d

--- app/javascript/components/intersectionObserver.js
+++ app/javascript/components/intersectionObserver.js
@@ -1,6 +1,9 @@
 const intersectionObserver =() =>{
 
 const skillSection = document.querySelector('.skill_block');
+const whiteboardR = document.querySelector('.whiteboard_right');
+const whiteboardL = document.querySelector('.whiteboard_left');
+const slider = document.querySelectorAll('.slide-in');
 const  options = {};
 if(skillSection) {
 const observer = new IntersectionObserver(function (entries, observer) {
@@ -12,5 +15,28 @@ options);
 
 observer.observe(skillSection);
 }
+
+if(whiteboardL) {
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+  };
+  const appearOnscroll = new IntersectionObserver(function(
+    entries,
+    appearOnscroll) {
+    entries.forEach(entry =>{
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('boardappear');
+        appearOnscroll.unobserve(entry.target);
+      }
+    })
+  },
+    appearOptions);
+}
+
+
+
 }
 export {intersectionObserver}
