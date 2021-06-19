d9282699eb84ae4c5c656c3aa4756f2b1c7340df
linlu liu <linlu96@yahoo.com>
2021-06-19 17:25:48 +0900

    d

--- app/javascript/components/burger.js
+++ app/javascript/components/burger.js
@@ -1,15 +1,17 @@
 const burgerMenu = () => {
 const menuBtn = document.querySelector('.hamburger');
-const links = document.querySelectorAll('.nav-link-sp li')
+const links = document.querySelector('.nav-link-sp')
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     console.log('d');
     menuBtn.classList.add('open');
+    links.classList.add('openmenu');
     menuOpen = true;
   } else {
     console.log('w')
     menuBtn.classList.remove('open');
+    links.classList.remove('openmenu')
     menuOpen = false;
   }
 });
