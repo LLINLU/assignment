b85a4a24310099aa635b9162be2638b319147142
linlu liu <linlu96@yahoo.com>
2021-08-21 20:32:06 +0900

    d

--- app/javascript/components/scrollSpy.js
+++ app/javascript/components/scrollSpy.js
@@ -1,1 +1,16 @@
-scrollSpy.js
+const scrollSpy = () => {
+$(document).on('scroll',function(){
+  var link = $('.navbar_cookpal a.dot');
+  var top = $(window).scrollTop();
+  $('.sec').each(function(){
+    var id = $(this).attr('id');
+    var height = $(this).height();
+    var pffset = $(this).offset().top-150;
+    if(top >= offset && top <offset + height){
+      link.removeClass('active');
+    }
+  })
+})
+}
+
+export { scrollSpy }
