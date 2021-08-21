910118070023f9f9c081b3c7e2e74dd1638fd8ca
linlu liu <linlu96@yahoo.com>
2021-08-21 21:10:12 +0900

    d

--- app/javascript/components/scrollSpy.js
+++ app/javascript/components/scrollSpy.js
@@ -2,12 +2,14 @@ const scrollSpy = () => {
 $(document).on('scroll',function(){
   var link = $('.navbar_cookpal a.dot');
   var top = $(window).scrollTop();
+  var offset = $(this).offset() - 150;
   $('.sec').each(function(){
     var id = $(this).attr('id');
     var height = $(this).height();
     var pffset = $(this).offset().top-150;
     if(top >= offset && top <offset + height){
       link.removeClass('active');
+      $('.navbar_cookpal').find('[data-scroll="' + id + '"]').addClass('active');
     }
   })
 })
