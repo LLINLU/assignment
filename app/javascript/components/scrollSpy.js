6a13c2f572bffde6ed12fcbcfead769397e0e0ee
linlu liu <linlu96@yahoo.com>
2021-08-22 10:38:27 +0900

    d

--- app/javascript/components/scrollSpy.js
+++ app/javascript/components/scrollSpy.js
@@ -1,23 +1,23 @@
-const scrollSpy = () => {
-$(document).on('scroll',function(){
-  var link = $('.navbar_cookpal a.dot');
-  var top = $(window).scrollTop();
-  var offset = $(this).offset() - 150;
-if (link) {
-  $('.sec').each(function(){
-    var id = $(this).attr('id');
-    var height = $(this).height();
-    var offset = $(this).offset().top-150;
-    if(top >= offset && top <offset + height){
-      link.removeClass('active');
-      $('.navbar_cookpal').find('[data-scroll="' + id + '"]').addClass('active');
-    }
-  })
-}
+// const scrollSpy = () => {
+// $(document).on('scroll',function(){
+//   var link = $('.navbar_cookpal a.dot');
+//   var top = $(window).scrollTop();
+//   var offset = $(this).offset() - 150;
+// if (link) {
+//   $('.sec').each(function(){
+//     var id = $(this).attr('id');
+//     var height = $(this).height();
+//     var offset = $(this).offset().top-150;
+//     if(top >= offset && top <offset + height){
+//       link.removeClass('active');
+//       $('.navbar_cookpal').find('[data-scroll="' + id + '"]').addClass('active');
+//     }
+//   })
+// }
 
-})
-}
+// })
+// }
 
-export { scrollSpy }
+// export { scrollSpy }
 
 
--- app/javascript/components/scrollTop.js
+++ app/javascript/components/scrollTop.js
@@ -1,20 +1,68 @@
+// const scrollTop = () => {
+// let btn = $('#button')
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '300');
+// });
+
+//  $(window).scroll(function() {
+//    if($(window).scrollTop() + $(window).height() > $(document).height() - 30) {
+//        btn.addClass('show-footer');
+//   } else {
+//     btn.removeClass('show-footer');
+
+//    }
+// });
+// }
+
+// export { scrollTop }
+
+
+// const scrollTop = () => {
+//   const btn = document.querySelector ('#button');
+//   if(btn){
+
+//   let btnPosition = btn.getBoundingClientRect().top;
+
+//   let screenPosition = window.innerHeight / 1.2;
+
+//     if (btnPosition < screenPosition) {
+
+//       btn.classList.add('show-footer');
+//     }
+//     else if (btnPosition >= screenPosition && true) {
+//       btn.classList.remove('show-footer');
+//     }
+//   }
+//   }
+
+// window.addEventListener('scroll',scrollTop)
+
+// export {scrollTop}
+
+
 const scrollTop = () => {
-let btn = $('#button')
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
+  const btn = document.querySelector ('#button');
+  if(btn){
 
- $(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() > $(document).height() - 30) {
-       btn.addClass('show-footer');
-  } else {
-    btn.removeClass('show-footer');
+  let btnPosition = btn.getBoundingClientRect().top;
 
-   }
-});
-}
+  let screenPosition = window.innerHeight;
+  let windowTop = window.pageYOffset;
+  let documentHeight= document.documentElement.scrollHeight;
 
-export { scrollTop }
+//    if($(window).scrollTop() + $(window).height() > $(document).height() - 30) {
 
+    if (windowTop + screenPosition > documentHeight - 30) {
 
+      btn.classList.add('show-footer');
+    }
+    else  {
+      btn.classList.remove('show-footer');
+    }
+  }
+  }
+
+window.addEventListener('scroll',scrollTop)
+
+export {scrollTop}
