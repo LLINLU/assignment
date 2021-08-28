bf3460fdaee08e35143ebe4ec3cddfa4941f5974
linlu liu <linlu96@yahoo.com>
2021-08-28 14:03:52 +0900

    d

--- app/javascript/components/scrollSpy.js
+++ app/javascript/components/scrollSpy.js
@@ -27,12 +27,14 @@ let navLinks = document.querySelectorAll('.navbar_cookpal a.dot');
 window.onscroll = () => {
   section.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop;
+    let offset = sec.offsetTop-100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
     if (top >= offset && top < offset + height) {
       navLinks.forEach(links => {
-
+        links.classList.remove('active');
+        document.querySelector('.navbar_cookpal a[href*=' + id + ']').classList.add
+        ('active');
       })
     }
 
