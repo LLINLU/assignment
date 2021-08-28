53637b14ea6309ff11fedac32ef639dd5c01b8b0
linlu liu <linlu96@yahoo.com>
2021-08-29 00:12:52 +0900

    d

--- app/javascript/components/scrollSpy.js
+++ app/javascript/components/scrollSpy.js
@@ -21,25 +21,53 @@
 // export { scrollSpy }
 
 
+// const scrollSpy = () => {
+// let section = document.querySelectorAll('.sec');
+// let navLinks = document.querySelectorAll('.navbar_cookpal a.dot');
+// window.onscroll = () => {
+//   section.forEach(sec => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop-300;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute('id');
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach(links => {
+//         links.classList.remove('active');
+//         document.querySelector('.navbar_cookpal a[href*=' + id + ']').classList.add
+//         ('active');
+//       })
+//     }
+
+//   })
+// }
+
+// }
+// export { scrollSpy }
+
+
 const scrollSpy = () => {
 let section = document.querySelectorAll('.sec');
 let navLinks = document.querySelectorAll('.navbar_cookpal a.dot');
 window.onscroll = () => {
+  let current = '';
   section.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop-300;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('.navbar_cookpal a[href*=' + id + ']').classList.add
-        ('active');
-      })
+    console.log(pageYOffset);
+    let sectionTop = sec.offsetTop-300;
+    let sectionHeight = sec.clientHeight;
+    if(pageYOffset >= sectionTop - sectionHeight / 3) {
+      current  = sec.getAttribute('id');
     }
-
   })
-}
+  navLinks.forEach (li => {
+    li.classList.remove('active');
+    if(li.classList.contains(current)){
+      li.classList.add('active')
+    }
+  })
 
 }
+}
+
 export { scrollSpy }
+
