cee03c5a77b52763bf13969aadd8f5d04c13c350
linlu liu <linlu96@yahoo.com>
2021-08-29 21:18:18 +0900

    d

--- app/javascript/components/navAppear.js
+++ app/javascript/components/navAppear.js
@@ -4,7 +4,7 @@ let navLinks = document.querySelector('.navbar_cookpal');
 let mastHead = document.querySelector('.about-page');
 if (mastHead){
 window.onscroll = () => {
-  if (pageYOffset > 200 ) {
+  if (pageYOffset > mastHead.clientHeight - 120) {
     navLinks.classList.add("navbar_cookpal_show");
   } else {
     navLinks.classList.remove("navbar_cookpal_show");
