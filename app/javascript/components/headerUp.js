fa8c3981b7975a09e8b0aa5b270889f261c8d1f5
linlu liu <linlu96@yahoo.com>
2021-08-31 23:41:36 +0900

    d

--- app/javascript/components/headerUp.js
+++ app/javascript/components/headerUp.js
@@ -1,7 +1,13 @@
 const headerUp= () => {
 let lastScrollTop = 0;
 let header =  document.getElementById ('PDP-header');
+if(header) {
+window.onscroll = () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
 
+
+}
+}
 
 }
