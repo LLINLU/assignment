59deee49a84c0d8821aba05895be71b7e26e7afe
linlu liu <linlu96@yahoo.com>
2021-09-02 22:24:21 +0900

    d

--- app/javascript/components/headerUp.js
+++ app/javascript/components/headerUp.js
@@ -1,19 +1,17 @@
 const headerUp= () => {
 let lastScrollTop = 0;
 let header =  document.getElementById ('PDP-header');
-if(header) {
+if (header) {
 window.onscroll = () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 if (scrollTop > lastScrollTop) {
   header.style.top = '-40px'；
 } else {
-  navbar.style.top = '-40px';
+  navbar.style.top = '0px';
 }
-
-}
-
-
-}
+lastScrollTop = scrollTop;
+    }
+  }
 }
 
 
