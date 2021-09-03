627e690b1d269c8734d119ebfd103b5b761158f6
linlu liu <linlu96@yahoo.com>
2021-09-03 22:22:44 +0900

    d

--- app/javascript/components/scrollEvent.js
+++ app/javascript/components/scrollEvent.js
@@ -7,12 +7,14 @@ let navLinks = document.querySelectorAll('.navbar_cookpal a.dot');
 let lastScrollTop = 0;
 let header =  document.querySelector ('.PDP-header');
 let logo = document.querySelector ('.logo_linlu');
+let uptoNext = document.querySelector('.cookpal_nav_about');
 if (mastHead){
 window.onscroll = () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop > lastScrollTop) {
   header.style.top = '-40px';
   logo.style.top = '-40px';
+  uptoNext.style.top = '-40px';
 
 } else {
   header.style.top = '0';
