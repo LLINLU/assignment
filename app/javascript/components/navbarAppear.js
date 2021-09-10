9e3470ca1b71521063769bc584591b7de65f62df
linlu liu <linlu96@yahoo.com>
2021-09-10 23:48:26 +0900

    d

--- app/javascript/components/navbarAppear.js
+++ app/javascript/components/navbarAppear.js
@@ -1,1 +1,20 @@
-navbarAppear.js
+const headerUp = () => {
+let lastScrollTop = 0;
+let header =  document.querySelector ('.PDP-header');
+let logo = document.querySelector ('.logo_linlu');
+if (header) {
+window.onscroll = () => {
+
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+if (scrollTop > lastScrollTop) {
+  header.style.top = '-40px';
+  logo.style.top = '-40px';
+
+} else {
+  header.style.top = '0';
+logo.style.top = '5px';
+}
+lastScrollTop = scrollTop;
+    }
+  }
+}
