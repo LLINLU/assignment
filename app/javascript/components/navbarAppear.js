1e6749d1a28b2dfe9f81f1d200cb094d07c74ed8
linlu liu <linlu96@yahoo.com>
2021-09-19 13:32:30 +0900

    d

--- app/javascript/components/navbarAppear.js
+++ app/javascript/components/navbarAppear.js
@@ -4,12 +4,12 @@ let navbar =  document.querySelector ('.navbar_home');
 let logo = document.querySelector ('.brand_logo');
 if (navbar) {
 window.onscroll = () => {
-  console.log('ddd');
+
 
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 if (scrollTop > lastScrollTop) {
   navbar.style.top = '-50px';
-  logo.style.visibility='visible';
+  // logo.style.visibility='visible';
 
 
   // logo.style.top = '-40px';
@@ -18,7 +18,7 @@ if (scrollTop > lastScrollTop) {
   navbar.style.top = '0px';
   navbar.style.height = '40px';
   // navbar.style.backgroundColor = '#F5E4EB';
-  logo.style.visibility='hidden';
+  // logo.style.visibility='hidden';
 
 // logo.style.top = '5px';
 }
