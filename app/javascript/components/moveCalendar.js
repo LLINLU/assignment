a3c66708abb1ed57c9083226e0edebb107561e52
linlu liu <linlu96@yahoo.com>
2021-09-23 00:05:57 +0900

    d

--- app/javascript/components/moveCalendar.js
+++ app/javascript/components/moveCalendar.js
@@ -1,8 +1,16 @@
 const moveCalendar = () => {
 const calendar = document.querySelector('.calendarMove');
-window.addEventListener
+window.addEventListener('scroll', ()=> {
+const scrolled = window.pageYOffset;
+const val = scrolled * 0.5;
+calendar.style.transform = `translateX(${0.45 * val}%)`;
+calendar.style. height = `${0.1 * val}px`;
+calendar.style. width = `${0.1 * val}px`;
 
-}
+
+
+})
+
 
 
 export {moveCalendar}
