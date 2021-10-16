57908c84de6a40160450025afb146c4922d7c33c
linlu liu <linlu96@yahoo.com>
2021-10-16 21:55:25 +0900

    d

---
+++ app/javascript/components/projectHeadline.js
@@ -0,0 +1,21 @@
+const headlineAppear = () => {
+  const introText = document.querySelector ('.text');
+  if(introText){
+  console.log(introText);
+  let introPosition = introText.getBoundingClientRect().top;
+  console.log(introPosition);
+  let screenPosition = window.innerHeight / 1.2;
+
+    if (introPosition < screenPosition) {
+      console.log('yes')
+      introText.classList.add('intro-appear');
+    }
+    else if (introPosition >= screenPosition && true) {
+      introText.classList.remove('intro-appear');
+    }
+  }
+  }
+
+window.addEventListener('scroll',scrollApear)
+
+export {scrollApear}
