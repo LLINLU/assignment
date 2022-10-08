d941f25e84993cb9b6f5dcb0f1dac365fd09fe93
linlu liu <linlu96@yahoo.com>
2022-10-08 17:31:12 +0900

    d

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/works', to: 'works#index'
   get '/works/cookpal', to: 'works#cookpal'
   get '/works/simplicruit', to: 'works#simplicruit'
+  get '/works/smartalert' , to: 'works#smartalert'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
